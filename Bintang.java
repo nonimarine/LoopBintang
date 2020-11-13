13c862c8055b37284421eb2a1b1ee438a0067e52
Noni Nindya <34081129+nonimarine@users.noreply.github.com>
2020-11-13 22:16:57 +0800

    Add files via upload

---
+++ Bintang.java
@@ -0,0 +1,51 @@
+package com.cilsy;
+
+public class Bintang {
+	public static void main(String[] args) {
+
+		System.out.println("Soal 1");
+		int a = 5;
+		for (int b = 1; b <= a; b++) {
+			for (int c = 4; c >= b; c--) {
+				System.out.print(' ');
+			}
+			for (int d = 1; d <= b; d++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+
+		System.out.println("Soal 2");
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("Soal 3");
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j <= 5; j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+
+		System.out.println("Soal 4");
+		int i, j, k;
+
+		for (i = 5; i >= 1; i--) {
+
+			for (j = 5; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (k = 1; k <= j; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
